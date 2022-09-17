07c5930ca61e78156db8d3c6f6f5ebc8d2cb2b7e
jspsegarra <jspsegarra86@gmail.com>
2022-09-17 17:12:34 +0200

    headache xD

---
+++ JS/go.js
@@ -0,0 +1,17 @@
+new Vue({
+    el: '#data',
+    data () {
+      return {
+        name: 0,
+        email: 0,
+        phone: 0,
+        message: 0,
+      }
+    },
+    methods: {
+        checkForm() {
+            
+        }
+    }
+  })
+  
