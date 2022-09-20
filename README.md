93956abb6da492f31da33fdda1568316f415d8b9
Marga <margarita.benitez.lopez@gmail.com>
2022-09-20 21:54:47 +0200

    [BCN-PT-072022] Marga Benítez

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Vue Form Lab
+
 Hola! 👋 Os dejamos la información para conectaros
+
 ```sh
 X-API-Key: a0wwnrex_JeRhBybn5iFYziStv9d2M6Mchd2b4B4H
 https://database.deta.sh/v1/a0wwnrex/contactmessages/items
--- js/index.js
+++ js/index.js
@@ -8,16 +8,31 @@ new Vue({
         name: "",
         phone: "",
       },
+      mailed: false,
     };
   },
   methods: {
-    sendData() {
+    sendData(data) {
       console.log(
         this.data.name,
         this.data.email,
-        this.data.mmessage,
+        this.data.message,
         this.data.phone
       );
+      const url = "https://database.deta.sh/v1/a0wwnrex/contactmessages/items";
+      const fetchParams = {
+        method: "Post",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          "X-API-Key": "a0wwnrex_JeRhBybn5iFYziStv9d2M6Mchd2b4B4H",
+        },
+        body: JSON.stringify({ item: this.data }),
+      };
+      fetch(url, fetchParams).then((response) => {
+        if (response.ok) this.mailed = true;
+        else return new Error("Error");
+      });
     },
   },
 });
