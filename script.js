cf5a728b57311daca4d483c610a04a4671ce3a61
ekatemi <ekatemi@gmail.com>
2022-09-20 15:42:46 +0200

    send form

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ new Vue({
     el: "#form",
     data() {
         return {
-            fname: "56y65y65",
+            fname: "",
             email: "",
             phone: "",
             message: "",
             nameErr: "",
             emailErr: "",
             phoneErr: "",
-            msgErr: ""
+            msgErr: "",
+            successMsg: "",
+            isSending: false
         }
     },
 
@@ -17,30 +19,70 @@ new Vue({
         handleSubmit(){
             console.log("form submitted");
             //validate name
-        /* /^([\w]{3,})+\s+([\w\s]{3,})+$/i
-        ([\w]{3,}) the first name should contain only letters and of length 3 or more
-        +\s the first name should be followed by a space
-        +([\w\s]{3,})+ the second name should contain only letters of length 3 or more and can be followed by other names or not
-        /i ignores the case of the letters. Can be uppercase or lowercase letters */
+            /* /^([\w]{3,})+\s+([\w\s]{3,})+$/i
+            ([\w]{3,}) the first name should contain only letters and of length 3 or more
+            +\s the first name should be followed by a space
+            +([\w\s]{3,})+ the second name should contain only letters of length 3 or more and can be followed by other names or not
+            /i ignores the case of the letters. Can be uppercase or lowercase letters */
             
             let fnameFormat = /^([\w]{3,})+\s+([\w\s]{3,})+$/i;
-            this.nameErr = this.fname.match(fnameFormat) ? "" : "Incorrect name format, please, enter valid full name";
-
+            let nameIsValid = this.fname.match(fnameFormat);
+            this.nameErr = nameIsValid ? "" : "Incorrect name format, please, enter valid full name";
+            
             //this.nameErr = this.fname !== "" ? "" : "Please, enter your full name"; ---this validate only if there is an input
             
             //validate email
             //regex to validate email contains @ and .
             let mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-            this.emailErr = this.email.match(mailFormat) ? "" : "Incorrect email format, should contain @ and .";
-
+            let emailIsValid = this.email.match(mailFormat);
+            this.emailErr = emailIsValid ? "" : "Incorrect email format, should contain @ and .";
+            
             //validate phone
-            this.phoneErr = this.phone.startsWith("+") ? "" : "Telephone should start with +";
+            let phoneIsValid = this.phone.startsWith("+");
+            this.phoneErr = phoneIsValid ? "" : "Telephone should start with +";
             //validate message
-            this.msgErr = this.message.length >= 20 ? "" : "The minimum length of message is 20 characters, add some more";
+            let messageIsValid = this.message.length >= 20;
+            this.msgErr = messageIsValid ? "" : "The minimum length of message is 20 characters, add some more";
 
+            
+            if (nameIsValid || emailIsValid || phoneIsValid || messageIsValid) {
 
+            const url = "https://database.deta.sh/v1/a0wwnrex/contactmessages/items";
+            const data = {
+                fname: this.fname,
+                email: this.email,
+                phone: this.phone,
+                message: this.message,
+              };
+        
+              const body = { item: data };
+
+            const fetchParams = {
+                method: "POST",
+                headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+                "X-API-Key": "a0wwnrex_JeRhBybn5iFYziStv9d2M6Mchd2b4B4H",
+                },
+                body: JSON.stringify(body),
+            };
+
+            fetch(url, fetchParams)
+                .then((response) => {
+                if (response.ok) return response.json();
+                })
+                //if all ok then
+                .then((json) => {
+                console.log(json);
+                this.successMsg = "Thank you for contact us!";
+                this.fname = "";
+                this.email = "";
+                this.phone = "";
+                this.message = "";
+                })
+                .catch((err) => console.log(err));
+            
+                }
+            }
         }
-
-}
-
   });
