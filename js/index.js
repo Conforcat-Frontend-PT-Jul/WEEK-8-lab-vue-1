7472bd7abdfa70de7c8f8669367b3c98ff70716a
Leandro Balaguer <leandro.balaguer@gmail.com>
2022-09-20 21:03:59 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,47 +1,40 @@
-
 new Vue({
-  el: 'form',
-  data() {
-    return {
-      name: '',
-      email: '',
-      phone: '',
-      message: ''
-    }
-  },
-
-  methods: {
-    onSubmit(data){
-      fetch('https://database.deta.sh/v1/a0wwnrex/contactmessages/items', {
-        method: 'POST',
-           headers: {
-          "Accept": "application/json",
-          "Content-Type": "application/json",
-          "X-API-Key": "a0wwnrex_JeRhBybn5iFYziStv9d2M6Mchd2b4B4H"
-        },
-        body: JSON.stringify({
-          name: this.name,
-          email: this.email,
-          phone: this.phone,
-          message: this.message
-        }),
-     
-      })
-        .then(response=> response.json())
-        .then(data => console.log(data));
+    el: '#form',
+    data: function() {
+      return {
+        name: '',
+        email: '',
+        phone: '',
+        message: '',
+        hi: "Contact us!",
+        f1: "Full Name*",
+        f2: "Email",
+        f3: "Phone",
+        f4: "Message"
+      }
     },
-  }
-    mounted() {
-      fetch('https://database.deta.sh/v1/a0wwnrex/contactmessages/items')
-        .then(response => {
-          if (response.ok) return response.json()
-          else return new Error(console.log("Error"))
-        })
-        .then(json => {
-          data = json
-        })
-          
-        }
-    }
 
-);
+    methods: {
+        go() {
+          fetch('https://database.deta.sh/v1/a0wwnrex/contactmessages/items',{
+            method:  'POST',
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                "X-API-Key": "a0wwnrex_JeRhBybn5iFYziStv9d2M6Mchd2b4B4H"
+              },
+            body: JSON.stringify({item:{
+              name: this.name,
+              phone:  this.phone,
+              email:   this.email,
+              message: this.message,
+            
+            }})
+          })
+            .then(response => response.json())
+            .then(data => console.log(data))
+        }
+      }
+
+    });
+    
