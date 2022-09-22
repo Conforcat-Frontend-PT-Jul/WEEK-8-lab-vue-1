20ef6380f628a5dd6b52911e510327046589dbab
jspsegarra <jspsegarra86@gmail.com>
2022-09-22 22:03:54 +0200

    Almost finished

--- JS/go.js
+++ JS/go.js
@@ -1,96 +1,105 @@
 new Vue({
-  el: '#data',
-  data() {
-    return {
-      user: {
-        items: {
-          name: '',
-          email: '',
-          phone: '',
-          message: ''
-        },
-        invalidName: false,
-        invalidEmail: false,
-        invalidPhone: false,
-        invalidMessage: false,
-        invalidForm: false,
-      }
+    el: '#data',
+    data() {
+        return {
+            user: {
+                name: '',
+                email: '',
+                phone: '',
+                message: ''
+            },
+            invalidName: false,
+            invalidEmail: false,
+            invalidPhone: false,
+            invalidMessage: false,
+            invalidForm: false,
+            confirmation: ''
+        }
     },
 
     methods: {
-      checkForm() {
-      async send() 
-          
-          this.validateName();
-          this.validateEmail();
-          this.validatePhone();
-          this.validateMessage();
-          if(this.checkForm()) {
-              this.invalidForm = false;
+        send() {
 
-              const url = 'https://database.deta.sh/v1/a0wwnrex/contactmessages/items';
-              const fetchParams = {
-                  method: 'POST',
-                  headers: {
-                      "Accept": "application/json",
-                      "Content-Type": "application/json",
-                      "X-API-Key": "a0wwnrex_JeRhBybn5iFYziStv9d2M6Mchd2b4B4H"
-                  },
-                  body: JSON.stringify({ item: this.info })
-              };
+            this.validateName();
+            this.validateEmail();
+            this.validatePhone();
+            this.validateMessage();
 
-              try {
-                  let response = await fetch(url, fetchParams);
-                  if (response.ok) {
-                      let result = await response.json();
-                      alert("Form was sent properly.");
-                      form.reset();
-                  } else {
-                      throw new Error(response.statusText);
-                  }
-              } catch (err) {
-                 alert(err);
-              }
-          } else {
-              this.invalidForm = true;
-          }
+            const url = 'https://database.deta.sh/v1/a0wwnrex/contactmessages/items';
+            const fetchParams = {
+                method: 'POST',
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json",
+                    "X-API-Key": "a0wwnrex_JeRhBybn5iFYziStv9d2M6Mchd2b4B4H"
+                },
+                body: JSON.stringify({ item: this.user })
+            };
+
+            fetch(url, fetchParams)
+            .then(response => {
+                if (response.ok) {
+                    
+                    this.confirmation = "Form was sent properly!",
+                    this.user= {
+                        name: '',
+                        email: '',
+                        phone: '',
+                        message: ''
+                    }
+                    return response.json()
+                }
+            })            
+        },
+
+        checkForm() {
+            return this.invalidName && this.invalidEmail && this.invalidPhone && this.invalidMessage
+        },
+
+        validateName() {
+            if (this.user.name) {
+                this.invalidName = true;
+            } else {
+                this.invalidName = false;
+            }
+        },
+
+        validateEmail() {
+            const regExEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            if (!this.user.email || !regExEmail.test(this.user.email)) {
+                this.invalidEmail = true;
+            } else {
+                this.invalidEmail = false;
+            }
+        },
+
+        validatePhone() {
+            const regExPhone = /(\+)[0-9]{11}/;
+            if (!this.user.phone || !regExPhone.test(this.user.phone)) {
+                this.invalidPhone = true;
+            } else {
+                this.invalidPhone = false;
+            }
+        },
+
+        validateMessage() {
+            const regExMessage = /^[\s\S]{50,}$/;
+            if (!this.user.message || !regExMessage.test(this.user.message)) {
+                this.invalidMessage = true;
+            } else {
+                this.invalidMessage = false;
+            }
+        },
+
+        reset() {
+            this.user.name = '';
+            this.user.email = '';
+            this.user.phone = '';
+            this.user.message = '';
+        },
+
+        hideConfirmation() {
+            this.confirmation = '';
         }
-      },
-      checkForm() {
-          return this.invalidName && this.invalidEmail && this.invalidPhone && this.invalidMessage
-      },
-      
-      validateName() {
-          if(this.info.name) {
-              this.invalidName = true;
-          } else {
-              this.invalidName = false;
-          }
-      },
-      validateEmail() {
-          const regExEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-          if(!this.info.email || !regExEmail.test(this.info.email)) {
-              this.invalidEmail = true;
-          } else {
-              this.invalidEmail = false;
-          }
-      },
-      validatePhone() {
-          const regExPhone = /(\+)[0-9]{11}/;
-          if(!this.info.phone || !regExPhone.test(this.info.phone)) {
-              this.invalidPhone = true;
-          } else {
-              this.invalidPhone = false;
-          }
-      },
-      validateMessage() {
-          const regExMessage = /^[\s\S]{50,150}$/;
-          if(!this.info.message || !regExMessage.test(this.info.message)) {
-              this.invalidMessage = true;
-          } else {
-              this.invalidMessage = false;
-          }
-      },
-      
-  }
+    }
 });
