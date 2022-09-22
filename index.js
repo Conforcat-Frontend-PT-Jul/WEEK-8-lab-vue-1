871dca8d97b7414118f90b5c6bfebc8157cb1173
claudiamurall <claudiamurall@gmail.com>
2022-09-22 21:03:40 +0200

    done

--- index.js
+++ index.js
@@ -1,61 +1,47 @@
 
-new VTTCue({
+new Vue({
     el: '#form',
-    data: () { 
+    data() { 
         return {
             name: '',
             email: '',
             phone: '',
             message: '',
-            errors {
-                name: '',
-                email: '',
-                phone: '',
-                message: ''  
-            };
-            enviado: '',
-        }
-    }
+        };
+    },
+
 
     methods: {
-        submit() {
-            this.errors.name = '';
-            this.errors.email = '';
-            this.errors.phone = '';
-            this.errors.message = '';
-
-            if (!this.name) {
-                this.errors.name = 'Por favor, rellena este campo.';
-                return false;
-              }
-
-            if (!this.email) {
-                this.errors.email = 'Por favor, introduce una dirección de correo electrónico válida.';
-                return false;
-              }
-        
-            if (!this.phone.startsWith("+")) {
-              this.errors.phone = 'Por favor, introduce un número de teléfono indicando el código de país. Por ejemplo: +34000000000';
-              return false;
+        submitForm: function(e) {
+            if(!this.name){
+                alert("Por favor, rellena este campo.");
+                 e.preventDefault();
             }
-        
-            if (this.message.length > 50) {
-              this.errors.message = 'El mensaje debe contener un máximo de 50 caracteres.';
-              return false;
+            if(!this.email){
+                alert("Por favor, introduce una dirección de correo electrónico válida.");
+                e.preventDefault();
+
             }
+            
+            if(!this.phone){
+                alert("Por favor, rellena este campo.");
+                e.preventDefault();
+            }
+            if(this.phone && this.phone[0] != '+'){
+                alert('Por favor, introduce un número de teléfono indicando el código de país. Por ejemplo: +34000000000');
+            }
+            if(!this.message){
+                alert("Por favor, rellena este campo.");
+                e.preventDefault();
+            }
+            if(message.length > 50){
+                alert('El mensaje debe contener un máximo de 50 caracteres.');             
+            }           
 
 
 
 const url = 'https://database.deta.sh/v1/a0wwnrex/contactmessages/items';
 
-const data = {
-    name: this.name,
-    email: this.email,
-    phone: this.phone,
-    message: this.message,
-    };
-    
-    
 const body = {
     item: data
     };
@@ -76,14 +62,16 @@ const fetchParams = {
                 if (response.ok) return response.json();
         })
         .then(json => {
-            console.log (json);
-            this.enviado = 'Gracias por tu mensaje. Ha sido enviado correctamente';
-            form.reset();
-        })
-        .catch(error => {
-            console.log(err);
-        }),
-    };
-}
+            if(result.errors){
+                result_api[0].innerHTML = 'error';
+                console.log('Error');
+                form.reset();
 
-})
+            }else{
+                result_api[0].innerText= 'Gracias Tu mensaje se ha enviado correctamente.';
+                form.reset();
+        }
+    })
+},
+    },
+});
