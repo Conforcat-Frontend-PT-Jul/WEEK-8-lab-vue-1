db79e5c1edd2adbd7a01f9402f0ade5f7e0b3102
laggart <laggart@gmail.com>
2022-09-22 21:38:21 +0200

    done

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const vueForm =
 new Vue({
     el: '#vueForm',
     data() {
@@ -6,88 +5,76 @@ new Vue({
             name: '',
             email: '',
             phone: '',
-            message: ''
-        }
-    },
+            message: '',                   
+        }      
+    },   
 
     methods: { 
-        formValidation() {
-            const validMail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/;
-            const validPhone = /^\+34(0-9){9})$/;
-            let postForm = true;
-
-        for (item in this.data) {
-            this.errors[item] = ''
+        postForm(event){ 
+        event.preventDefault()
+        const body = {
+            name: this.name,
+            email: this.email,
+            phone: this.phone,
+            message: this.message
         }
+        console.log(body)
+        const fetchParams = {
+            method: "POST",
+            headers: {
+            "Accept": 'application/json',
+            "Content-Type": 'application/json',
+            "X-API-Key":'a0wwnrex_JeRhBybn5iFYziStv9d2M6Mchd2b4B4H'
+            },
+            body: JSON.stringify({ item: body })
+            };
+            fetch("https://database.deta.sh/v1/a0wwnrex/contactmessages/items", fetchParams)
+            .then(response => {
+                if (response.ok) return response.json(); 
+            })
+                                        
+            .then(json => {
+            console.log(json);
+            })
+                                        
+            .catch(error => {
+            console.log(error)
+            });
+                                        
+            vueForm.reset()                                 
+                            
+                            
+        }
+    },
+})
+
+
+
 
         
-        }
-
-        onSubmit(e) {
-            e.preventDefault()
-            if(!this.name || !this.email || !this.phone || !this.message){
-                alert('All fields must be completed')
-            };
-        function mailValidate(email) {
-            if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/.test(valor)){
-                alert("The email adress" + email + " is correct.")
-                    }    
-        }       
-            
-
-            
-
-
-
-
-
-        }
-    }
-
-
-
-
-
-
-
-
-    
-}
+        
+                     
+                    
+                     
+                     
+                     
+                     
     
 
-const data = {
-    name: '',
-    email: '',
-    phone: '',
-    message: ''
-}
+/* formValidation() {
+            const validMail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/;
+            const validPhone = /^\+34(0-9){9}$/;
+            let postForm = true;
 
+            for (item in this.data) {
+                if (!this.name || !this.email || !this.phone || !this.message)
+                this.errors[item] = 'All fields must be completed', postForm = false
+                else {
+                    if (item === 'email' && !validMail.test(this.data[item])) this.errors[item] = 'Please insert a valid mail', postForm = false
+                    if (item === 'phone' && !validPhone.test(this.data[item])) this.errors[item] = 'please use a valid number +34 XXX XXX XXX', postForm = false
+                    if (item === 'message' && this.data[item].length < 50) this.errors[item] = 'please enter at least 50 characters', postForm = false
+                 }; */ 
 
-
-const fetchParams = {
-    method: "POST",
-    headers: {
-        "Accept": 'application/json',
-        "Content-Type": 'application/json',
-        "X-API-Key":'a0wwnrex_JeRhBybn5iFYziStv9d2M6Mchd2b4B4H'
-    },
-    body: JSON.stringify({ item: data })
-};
-
-fetch(url, fetchParams)
-    .then(response => {
-        if (response.ok) return response.json(); 
-    })
-
-    .then(json => {
-        console.log(json);
-    })
-
-    .catch(error => {
-        console.log(error)
-    });
-
-    form.reset()
 
 
 
